79be8bc8ebf21ac1d32c4132ca94570deef5d6ff
Sami K <sami.karna@gmail.com>
2018-07-10 18:15:44 +0300

    Add solution 2.8

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -5,9 +5,13 @@ class App extends React.Component {
         super(props)
         this.state = {
             persons: [
-                { name: 'Arto Hellas' }
+                {
+                    name: 'Arto Hellas',
+                    number: '040-123456'
+                }
             ],
-            newName: ''
+            newName: '',
+            newNumber: ''
         }
     }
 
@@ -15,12 +19,16 @@ class App extends React.Component {
         this.setState({ newName: event.target.value })
     }
 
+    handleNumberChange = (event) => {
+        this.setState({ newNumber: event.target.value })
+    }
+
     isNameInCatalog = (name) => {
         const findPerson = (person) => {
             return person.name === name;
         }
-       
-        return (this.state.persons.findIndex(findPerson) !== -1) 
+
+        return (this.state.persons.findIndex(findPerson) !== -1)
     }
 
     addName = (event) => {
@@ -28,7 +36,10 @@ class App extends React.Component {
         if (this.isNameInCatalog(this.state.newName)) {
             return
         }
-        const person = { name: this.state.newName }
+        const person = {
+            name: this.state.newName,
+            number: this.state.newNumber
+        }
         const persons = this.state.persons.concat(person)
         this.setState({ persons: persons })
     }
@@ -43,15 +54,23 @@ class App extends React.Component {
                             onChange={this.handleNameChange} />
                     </div>
                     <div>
+                        numero: <input value={this.state.newNumber}
+                            onChange={this.handleNumberChange} />
+                    </div>
+                    <div>
                         <button type="submit">lisää</button>
                     </div>
                 </form>
                 <h2>Numerot</h2>
-                <ul>
+                <table>
+                    <tbody>
                     {this.state.persons.map(person =>
-                        <li key={person.name}>
-                            {person.name} </li>)}
-                </ul>
+                        <tr key={person.name}>
+                            <td>{person.name} </td>
+                            <td>{person.number}</td>
+                        </tr>)}
+                        </tbody>
+                </table>
 
             </div>
         )
