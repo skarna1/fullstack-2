703b86d5525688affaadae95a021fcddfd477b2a
Sami K <sami.karna@gmail.com>
2018-07-10 18:51:53 +0300

    Add solution 2.10

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -1,18 +1,75 @@
 import React from 'react';
 
+const FilterField = ({ filter, handleFilterChange }) => {
+    return (
+        <div>
+            rajaa näytettäviä: <input value={filter}
+                onChange={handleFilterChange} />
+        </div>
+    )
+}
+
+const NewPersonForm = (props) => {
+    return (
+        <div>
+            <h2>Lisää uusi</h2>
+            <form onSubmit={props.addName}>
+                <div>
+                    nimi: <input value={props.newName}
+                        onChange={props.handleNameChange} />
+                </div>
+                <div>
+                    numero: <input value={props.newNumber}
+                        onChange={props.handleNumberChange} />
+                </div>
+                <div>
+                    <button type="submit">lisää</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+const Person = ({ person }) => {
+    return (
+        <tr>
+            <td>{person.name} </td>
+            <td>{person.number}</td>
+        </tr>
+    )
+}
+
+const PersonList = ({ persons }) => {
+    return (
+        <div>
+            <h2>Numerot</h2>
+            <table>
+                <tbody>
+                    {persons.map(person =>
+                        <Person key={person.name} person={person} />)}
+                </tbody>
+            </table>
+        </div>
+    )
+}
 class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
             persons: [
-                {
-                    name: 'Arto Hellas',
-                    number: '040-123456'
-                }
+                { name: 'Arto Hellas', number: '040-123456' },
+                { name: 'Martti Tienari', number: '040-123456' },
+                { name: 'Arto Järvinen', number: '040-123456' },
+                { name: 'Lea Kutvonen', number: '040-123456' }
             ],
             newName: '',
-            newNumber: ''
+            newNumber: '',
+            filter: ''
         }
+    }
+
+    handleFilterChange = (event) => {
+        this.setState({ filter: event.target.value })
     }
 
     handleNameChange = (event) => {
@@ -44,34 +101,24 @@ class App extends React.Component {
         this.setState({ persons: persons })
     }
 
+
+
     render() {
+        const getRenderedPersons = () =>
+            this.state.persons.filter(person =>
+                person.name.toLowerCase().startsWith(this.state.filter.toLowerCase()))
         return (
             <div>
                 <h2>Puhelinluettelo</h2>
-                <form onSubmit={this.addName}>
-                    <div>
-                        nimi: <input value={this.state.newName}
-                            onChange={this.handleNameChange} />
-                    </div>
-                    <div>
-                        numero: <input value={this.state.newNumber}
-                            onChange={this.handleNumberChange} />
-                    </div>
-                    <div>
-                        <button type="submit">lisää</button>
-                    </div>
-                </form>
-                <h2>Numerot</h2>
-                <table>
-                    <tbody>
-                    {this.state.persons.map(person =>
-                        <tr key={person.name}>
-                            <td>{person.name} </td>
-                            <td>{person.number}</td>
-                        </tr>)}
-                        </tbody>
-                </table>
-
+                <FilterField filter={this.state.filter}
+                    handleFilterChange={this.handleFilterChange} />
+                <NewPersonForm
+                    newName={this.state.newName}
+                    newNumer={this.state.newNumber}
+                    handleNameChange={this.handleNameChange}
+                    handleNumberChange={this.handleNumberChange}
+                    addName={this.addName} />
+                <PersonList persons={getRenderedPersons()} />
             </div>
         )
     }
